74e76231b0f57afae48ee2a38b2ad4e35d8e4377
nyxb <contact@nyxb.email>
2023-10-25 10:19:56 +0200

    🚀 feat(nyxbi.mjs): add new executable script for running main function 🎨 style(commands): improve code readability by adjusting indentation and line breaks 🔧 refactor(init.ts): restructure code for better readability and maintainability

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -9,173 +9,173 @@ import { defineCommand } from 'citty'
 
 import { sharedArgs } from './_shared'
 
-const DEFAULT_REGISTRY =
-  'https://raw.githubusercontent.com/nuxt/starter/templates/templates'
+const DEFAULT_REGISTRY
+  = 'https://raw.githubusercontent.com/nuxt/starter/templates/templates'
 const DEFAULT_TEMPLATE_NAME = 'v3'
 
 export default defineCommand({
-  meta: {
-    name: 'init',
-    description: 'Initialize a fresh project',
-  },
-  args: {
-    ...sharedArgs,
-    dir: {
-      type: 'positional',
-      description: 'Project directory',
-      default: '',
-    },
-    template: {
-      type: 'string',
-      alias: 't',
-      description: 'Template name',
-    },
-    force: {
-      type: 'boolean',
-      alias: 'f',
-      description: 'Override existing directory',
-    },
-    offline: {
-      type: 'boolean',
-      description: 'Force offline mode',
-    },
-    preferOffline: {
-      type: 'boolean',
-      description: 'Prefer offline mode',
-    },
-    install: {
-      type: 'boolean',
-      default: true,
-      description: 'Skip installing dependencies',
-    },
-    gitInit: {
-      type: 'boolean',
-      description: 'Initialize git repository',
-    },
-    shell: {
-      type: 'boolean',
-      description: 'Start shell after installation in project directory',
-    },
-    packageManager: {
-      type: 'string',
-      description: 'Package manager choice (npm, pnpm, yarn, bun)',
-    },
-  },
-  async run(ctx) {
-    const cwd = resolve(ctx.args.cwd || '.')
+   meta: {
+      name: 'init',
+      description: 'Initialize a fresh project',
+   },
+   args: {
+      ...sharedArgs,
+      dir: {
+         type: 'positional',
+         description: 'Project directory',
+         default: '',
+      },
+      template: {
+         type: 'string',
+         alias: 't',
+         description: 'Template name',
+      },
+      force: {
+         type: 'boolean',
+         alias: 'f',
+         description: 'Override existing directory',
+      },
+      offline: {
+         type: 'boolean',
+         description: 'Force offline mode',
+      },
+      preferOffline: {
+         type: 'boolean',
+         description: 'Prefer offline mode',
+      },
+      install: {
+         type: 'boolean',
+         default: true,
+         description: 'Skip installing dependencies',
+      },
+      gitInit: {
+         type: 'boolean',
+         description: 'Initialize git repository',
+      },
+      shell: {
+         type: 'boolean',
+         description: 'Start shell after installation in project directory',
+      },
+      packageManager: {
+         type: 'string',
+         description: 'Package manager choice (npm, pnpm, yarn, bun)',
+      },
+   },
+   async run(ctx) {
+      const cwd = resolve(ctx.args.cwd || '.')
 
-    // Get template name
-    const templateName = ctx.args.template || DEFAULT_TEMPLATE_NAME
+      // Get template name
+      const templateName = ctx.args.template || DEFAULT_TEMPLATE_NAME
 
-    if (typeof templateName !== 'string') {
-      consola.error('Please specify a template!')
-      process.exit(1)
-    }
-
-    // Download template
-    let template: DownloadTemplateResult
-
-    try {
-      template = await downloadTemplate(templateName, {
-        dir: ctx.args.dir,
-        cwd,
-        force: Boolean(ctx.args.force),
-        offline: Boolean(ctx.args.offline),
-        preferOffline: Boolean(ctx.args.preferOffline),
-        registry: process.env.NYXBI_INIT_REGISTRY || DEFAULT_REGISTRY,
-      })
-    } catch (err) {
-      if (process.env.DEBUG) {
-        throw err
+      if (typeof templateName !== 'string') {
+         consola.error('Please specify a template!')
+         process.exit(1)
       }
-      consola.error((err as Error).toString())
-      process.exit(1)
-    }
 
-    // Resolve package manager
-    const packageManagerOptions: PackageManagerName[] = [
-      'npm',
-      'pnpm',
-      'yarn',
-      'bun',
-    ]
-    const packageManagerArg = ctx.args.packageManager as PackageManagerName
-    const selectedPackageManager = packageManagerOptions.includes(
-      packageManagerArg,
-    )
-      ? packageManagerArg
-      : await consola.prompt('Which package manager would you like to use?', {
-          type: 'select',
-          options: packageManagerOptions,
-        })
-
-    // Get relative project path
-    const relativeProjectPath = relative(process.cwd(), template.dir)
-
-    // Write .nuxtrc with `shamefully-hoist=true` for pnpm
-    if (selectedPackageManager === 'pnpm') {
-      await writeFile(`${relativeProjectPath}/.npmrc`, 'shamefully-hoist=true')
-    }
-
-    // Install project dependencies
-    // or skip installation based on the '--no-install' flag
-    if (ctx.args.install === false) {
-      consola.info('Skipping install dependencies step.')
-    } else {
-      consola.start('Installing dependencies...')
+      // Download template
+      let template: DownloadTemplateResult
 
       try {
-        await installDependencies({
-          cwd: relativeProjectPath,
-          packageManager: {
-            name: selectedPackageManager,
-            command: selectedPackageManager,
-          },
-        })
-      } catch (err) {
-        if (process.env.DEBUG) {
-          throw err
-        }
-        consola.error((err as Error).toString())
-        process.exit(1)
+         template = await downloadTemplate(templateName, {
+            dir: ctx.args.dir,
+            cwd,
+            force: Boolean(ctx.args.force),
+            offline: Boolean(ctx.args.offline),
+            preferOffline: Boolean(ctx.args.preferOffline),
+            registry: process.env.NYXBI_INIT_REGISTRY || DEFAULT_REGISTRY,
+         })
+      }
+      catch (err) {
+         if (process.env.DEBUG)
+            throw err
+
+         consola.error((err as Error).toString())
+         process.exit(1)
       }
 
-      consola.success('Installation completed.')
-    }
+      // Resolve package manager
+      const packageManagerOptions: PackageManagerName[] = [
+         'npm',
+         'pnpm',
+         'yarn',
+         'bun',
+      ]
+      const packageManagerArg = ctx.args.packageManager as PackageManagerName
+      const selectedPackageManager = packageManagerOptions.includes(
+         packageManagerArg,
+      )
+         ? packageManagerArg
+         : await consola.prompt('Which package manager would you like to use?', {
+            type: 'select',
+            options: packageManagerOptions,
+         })
 
-    if (ctx.args.gitInit === undefined) {
-      ctx.args.gitInit = await consola.prompt('Initialize git repository?', {
-        type: 'confirm',
-      })
-    }
-    if (ctx.args.gitInit) {
-      consola.info('Initializing git repository...\n')
-      const { execaCommand } = await import('execa')
-      await execaCommand(`git init ${template.dir}`, {
-        stdio: 'inherit',
-      }).catch((err) => {
-        consola.warn(`Failed to initialize git repository: ${err}`)
-      })
-    }
+      // Get relative project path
+      const relativeProjectPath = relative(process.cwd(), template.dir)
 
-    // Display next steps
-    consola.log(
+      // Write .nuxtrc with `shamefully-hoist=true` for pnpm
+      if (selectedPackageManager === 'pnpm')
+         await writeFile(`${relativeProjectPath}/.npmrc`, 'shamefully-hoist=true')
+
+      // Install project dependencies
+      // or skip installation based on the '--no-install' flag
+      if (ctx.args.install === false) {
+         consola.info('Skipping install dependencies step.')
+      }
+      else {
+         consola.start('Installing dependencies...')
+
+         try {
+            await installDependencies({
+               cwd: relativeProjectPath,
+               packageManager: {
+                  name: selectedPackageManager,
+                  command: selectedPackageManager,
+               },
+            })
+         }
+         catch (err) {
+            if (process.env.DEBUG)
+               throw err
+
+            consola.error((err as Error).toString())
+            process.exit(1)
+         }
+
+         consola.success('Installation completed.')
+      }
+
+      if (ctx.args.gitInit === undefined) {
+         ctx.args.gitInit = await consola.prompt('Initialize git repository?', {
+            type: 'confirm',
+         })
+      }
+      if (ctx.args.gitInit) {
+         consola.info('Initializing git repository...\n')
+         const { execaCommand } = await import('execa')
+         await execaCommand(`git init ${template.dir}`, {
+            stdio: 'inherit',
+         }).catch((err) => {
+            consola.warn(`Failed to initialize git repository: ${err}`)
+         })
+      }
+
+      // Display next steps
+      consola.log(
       `\n✨ Nuxt project has been created with the \`${template.name}\` template. Next steps:`,
-    )
+      )
 
-    const nextSteps = [
-      !ctx.args.shell &&
-        relativeProjectPath.length > 1 &&
-        `\`cd ${relativeProjectPath}\``,
+      const nextSteps = [
+         !ctx.args.shell
+        && relativeProjectPath.length > 1
+        && `\`cd ${relativeProjectPath}\``,
       `Start development server with \`${selectedPackageManager} run dev\``,
-    ].filter(Boolean)
+      ].filter(Boolean)
 
-    for (const step of nextSteps) {
-      consola.log(` › ${step}`)
-    }
+      for (const step of nextSteps)
+         consola.log(` › ${step}`)
 
-    if (ctx.args.shell) {
-      startShell(template.dir)
-    }
-  },
+      if (ctx.args.shell)
+         startShell(template.dir)
+   },
 })

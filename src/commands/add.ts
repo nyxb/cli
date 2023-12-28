8e3db393487789b99255f46b5c7e23b0149aacdc
nyxb <contact@nyxb.email>
2023-12-28 19:13:32 +0100

    ✨ feat(commands/add.ts): add new command to create template files
    This new command allows users to generate new files based on predefined templates, with options to override existing files and specify the name of the new file. It includes validation for template names and checks for the existence of necessary directories, creating them if they do not exist. This feature enhances the developer experience by automating the creation of common file patterns.

---
+++ src/commands/add.ts
@@ -0,0 +1,94 @@
+import { existsSync, promises as fsp } from 'node:fs'
+import { dirname, resolve } from 'pathe'
+import { consola } from 'consola'
+import { defineCommand } from 'citty'
+import { loadKit } from '../utils/kit'
+import { templates } from '../utils/templates'
+import { sharedArgs } from './_shared'
+
+export default defineCommand({
+   meta: {
+      name: 'add',
+      description: 'Create a new template file.',
+   },
+   args: {
+      ...sharedArgs,
+      force: {
+         type: 'boolean',
+         description: 'Override existing file',
+      },
+      template: {
+         type: 'positional',
+         required: true,
+         valueHint: Object.keys(templates).join('|'),
+      },
+      name: {
+         type: 'positional',
+         required: true,
+         valueHint: 'name',
+      },
+   },
+   async run(ctx) {
+      const cwd = resolve(ctx.args.cwd || '.')
+
+      const template = ctx.args.template
+      const name = ctx.args.name
+
+      // Validate template name
+      if (!templates[template]) {
+         consola.error(
+        `Template ${template} is not supported. Possible values: ${Object.keys(templates).join(', ')}`,
+         )
+         process.exit(1)
+      }
+
+      // Validate options
+      if (!name) {
+         consola.error('name argument is missing!')
+         process.exit(1)
+      }
+
+      // Recognize the project type (Nuxt or Vite)
+      const isNuxt = existsSync(resolve(cwd, 'nuxt.config.js')) || existsSync(resolve(cwd, 'nuxt.config.ts'))
+
+      // Loading the configuration for Nuxt projects only
+      let basePath = cwd
+      if (isNuxt) {
+         const kit = await loadKit(cwd)
+         const config = await kit.loadNuxtConfig({ cwd })
+         basePath = config.srcDir || cwd
+      }
+
+      // Check whether a 'src' folder exists
+      if (!isNuxt && existsSync(resolve(cwd, 'src')))
+         basePath = resolve(cwd, 'src')
+
+      // Resolve template
+      const res = templates[template]({ name, args: ctx.args })
+
+      // Resolve full path to generated file
+      const path = resolve(basePath, res.path)
+
+      // Ensure not overriding user code
+      if (!ctx.args.force && existsSync(path)) {
+         consola.error(
+        `File exists: ${path}. Use --force to override or use a different name.`,
+         )
+         process.exit(1)
+      }
+
+      // Ensure parent directory exists
+      const parentDir = dirname(path)
+      if (!existsSync(parentDir)) {
+         consola.info('Creating directory', parentDir)
+         if (template === 'page')
+            consola.info('This enables vue-router functionality!')
+
+         await fsp.mkdir(parentDir, { recursive: true })
+      }
+
+      // Write file
+      await fsp.writeFile(path, `${res.contents.trim()}\n`)
+      consola.info(`🪄 Generated a new ${template} in ${path}`)
+   },
+})
